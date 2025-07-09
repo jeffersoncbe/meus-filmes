cae2d672e864357ed40a321d45c941f5c73c0896
JEFFERSON DIEGO LIMA DA SILVA <jeffersonsilva0425@digitalcollege.internal>
2025-07-08 21:36:25 -0300

    Teste de API Autenticada

---
+++ script.js
@@ -0,0 +1,13 @@
+const options ={
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzJhMWU5NzA1ZmQ5OThlYWRkYjY1ZWY0MzhmZWMyNiIsIm5iZiI6MTc1MjAxODg1NC43MzYsInN1YiI6IjY4NmRhZmE2MjkxMzA3NjU2MzcwM2E1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rJXlKvSxImiQKdosZS6WivzKR7R064r6k2jvDIYgg5c'
+    }
+}
+
+fetch('https://api.themoviedb.org/3/authentication', options)
+.then(response => response.json())
+.then(data =>{
+    console.log(data);
+})
